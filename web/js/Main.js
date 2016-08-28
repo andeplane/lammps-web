149d5b092ae21963d0cdb317c10f9d3e72f2b703
Anders Hafreager <andershaf@gmail.com>
2016-08-28 22:26:07 +0200

    Moved loading code to Main.js

---
+++ web/js/Main.js
@@ -0,0 +1,94 @@
+var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+	lineNumbers: true,
+	styleActiveLine: true,
+	matchBrackets: true,
+	viewportMargin: Infinity
+});
+var input = document.getElementById("select");
+
+editor.setOption("theme", "monokai");
+
+var choice = (location.hash && location.hash.slice(1)) ||
+           (document.location.search &&
+            decodeURIComponent(document.location.search.slice(1)));
+if (choice) {
+	input.value = choice;
+	editor.setOption("theme", choice);
+}
+CodeMirror.on(window, "hashchange", function() {
+	var theme = location.hash.slice(1);
+	if (theme) { input.value = theme; selectTheme(); }
+});
+
+// Build GUI
+var toolbar = document.getElementById( 'toolbar' );
+var buttonUpdate = document.createElement( 'button' );
+buttonUpdate.className = 'button';
+
+buttonUpdate.appendChild( document.createTextNode( 'run lammps script' ) );
+
+buttonUpdate.addEventListener( 'click', function ( event ) {
+	var value = editor.getValue();
+	currentScript = value
+	runScript()
+}, false );
+toolbar.appendChild( buttonUpdate );
+
+var buttonSave = document.createElement( 'button' );
+buttonSave.className = 'button';
+buttonSave.textContent = 'save';
+buttonSave.addEventListener( 'click', function ( event ) {
+	save();
+}, false );
+toolbar.appendChild( buttonSave );
+
+var buttonHide = document.createElement( 'button' );
+buttonHide.className = 'button';
+buttonHide.textContent = 'hide code';
+buttonHide.addEventListener( 'click', function ( event ) {
+	var code = document.getElementById( 'editor' );
+	if ( code.style.display === '' ) {
+		buttonHide.textContent = 'show code';
+		code.style.display = 'none';
+		buttonUpdate.style.display = 'none';
+		buttonSave.style.display = 'none';
+	} else {
+		buttonHide.textContent = 'hide code';
+		code.style.display = '';
+		buttonUpdate.style.display = '';
+		buttonSave.style.display = '';
+	}
+}, false );
+toolbar.appendChild( buttonHide );
+
+document.addEventListener( 'keydown', function ( event ) {
+	if ( event.keyCode === 83 && ( event.ctrlKey === true || event.metaKey === true ) ) {
+		event.preventDefault();
+		save();
+	}
+
+	if ( event.keyCode === 82 && ( event.ctrlKey === true || event.metaKey === true ) ) {
+		event.preventDefault();
+		var value = editor.getValue();
+		currentScript = value
+		runScript()
+	}
+
+	if ( event.keyCode === 27 ) {
+		var code = document.getElementById( 'editor' );
+		if ( code.style.display === '' ) {
+			buttonHide.textContent = 'show code';
+			code.style.display = 'none';
+			buttonUpdate.style.display = 'none';
+			buttonSave.style.display = 'none';
+		} else {
+			buttonHide.textContent = 'hide code';
+			code.style.display = '';
+			buttonUpdate.style.display = '';
+			buttonSave.style.display = '';
+		}
+	}
+
+}, false );
+
+init();
